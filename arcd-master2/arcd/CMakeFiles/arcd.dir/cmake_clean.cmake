516366cfb328f32d11786285b732e4aee00fdaeb
kychen <115061097+nihowmalalula@users.noreply.github.com>
2023-01-10 20:44:32 +0800

    先傳一部分

---
+++ arcd-master2/arcd/CMakeFiles/arcd.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/arcd.dir/arcd.c.o"
+  "libarcd.pdb"
+  "libarcd.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/arcd.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
